b24f12c89de94bcd37cecbd420e86fda7eb9600d
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2024-09-30 12:46:11 +0200

    Added function to see datamuse array on canvas and added conversation with chatgpt.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
+/*  
+
+I have used chatGPT for help in this project. Beneatch is a link to the conversation that i have 
+had with chatGPT. -->
+
+https://chatgpt.com/share/66fa8143-b374-800d-a64a-07d768cfeb03
+
+*/
+
+let words = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {}
+function draw() {
+  displayWords();
+}
 
 // This function will be triggered by the button click in the HTML
 function searchDatamuse() {
@@ -19,7 +32,19 @@ function datamuseFetch(query) {
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
+      words = json;
       // Process or display the results here
     })
     .catch((err) => console.error("API request failed:", err));
+}
+
+function displayWords() {
+  fill(255);
+  textSize(24);
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i].word;
+
+    text(word, width / 2, 100 + i * 40);
+  }
 }
